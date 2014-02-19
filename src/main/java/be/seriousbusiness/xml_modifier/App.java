999f727243d5848dc4e51615a3c54676461b02be
stefan borghys <stefan.borghys@seriousbusiness.be>
2014-02-20 00:01:02 +0100

    Added Maven pom.xml
    Added Maven pom.xml containing dependencies on slf4j, logback and junit.

---
+++ src/main/java/be/seriousbusiness/xml_modifier/App.java
@@ -0,0 +1,13 @@
+package be.seriousbusiness.xml_modifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
